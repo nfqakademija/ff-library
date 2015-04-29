79c3062d657da71fa15021ada1c680ec2a64c609
Pavelas <epavelas@gmail.com>
2015-04-29 06:57:59 +0300

    Add book entities and display results from database.

--- src/F4/LibraryBundle/Controller/PageController.php
+++ src/F4/LibraryBundle/Controller/PageController.php
@@ -4,11 +4,25 @@
 namespace F4\LibraryBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use F4\LibraryBundle\Entity\Enquiry;
+use F4\LibraryBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('F4LibraryBundle:Page:index.html.twig');
+        $em = $this->getDoctrine()
+            ->getManager();
+
+        $books = $em->createQueryBuilder()
+            ->select('b')
+            ->from('F4LibraryBundle:Book',  'b')
+            ->addOrderBy('b.releaseDate', 'DESC')
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('F4LibraryBundle:Page:index.html.twig', array(
+            'books' => $books
+        ));
     }
 }
