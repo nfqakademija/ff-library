3079ccf744eb45cbdd214374899db6142bc4fb78
Pavelas <epavelas@gmail.com>
2015-05-03 04:46:42 +0300

    Install DoctrineMigrationBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new F4\LibraryBundle\F4LibraryBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
