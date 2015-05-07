064712ae5075cbd48a04441f9b25ed0db123cbe3
Pavelas <epavelas@gmail.com>
2015-05-07 03:37:37 +0300

    Add Review Entity.

---
+++ src/F4/LibraryBundle/Entity/Review.php
@@ -0,0 +1,212 @@
+<?php
+namespace F4\LibraryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="F4\LibraryBundle\Entity\Repository\ReviewRepository")
+ * @ORM\Table(name="review")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Review
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $user;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $rating;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $review;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Book", inversedBy="reviews")
+     * @ORM\JoinColumn(name="book_id", referencedColumnName="id")
+     */
+    protected $book;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated;
+
+    public function __construct()
+    {
+        $this->setCreated(new \DateTime());
+        $this->setUpdated(new \DateTime());
+    }
+
+    /**
+     * @ORM\preUpdate
+     */
+    public function setUpdatedValue()
+    {
+        $this->setUpdated(new \DateTime());
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param string $user
+     * @return Review
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return string 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set rating
+     *
+     * @param integer $rating
+     * @return Review
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    /**
+     * Get rating
+     *
+     * @return integer 
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * Set review
+     *
+     * @param string $review
+     * @return Review
+     */
+    public function setReview($review)
+    {
+        $this->review = $review;
+
+        return $this;
+    }
+
+    /**
+     * Get review
+     *
+     * @return string 
+     */
+    public function getReview()
+    {
+        return $this->review;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Review
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     * @return Review
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime 
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * Set book
+     *
+     * @param \F4\LibraryBundle\Entity\Book $book
+     * @return Review
+     */
+    public function setBook(\F4\LibraryBundle\Entity\Book $book = null)
+    {
+        $this->book = $book;
+
+        return $this;
+    }
+
+    /**
+     * Get book
+     *
+     * @return \F4\LibraryBundle\Entity\Book 
+     */
+    public function getBook()
+    {
+        return $this->book;
+    }
+}
